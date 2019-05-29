e6ebe7db2fcfabbf3e18721c6437ca44fa459fb2
gn3112 <georgesnomicos@gmail.com>
2019-05-29 10:31:22 +0100

    time limit env

--- env.py
+++ env.py
@@ -5,14 +5,20 @@ import torch
 class Env():
   def __init__(self):
     self._env = environment()
-
+    self.n_step = 0
   def reset(self):
     self._env.reset_robot_position(random_=True)
     self._env.reset_target_position(random_=False)
     state = self._env.get_obs()
-    return torch.tensor(state, dtype=torch.float32).unsqueeze(dim=0)
+    return torch.tensor(state, dtype=torch.float32)
 
   def step(self, action):
-    reward, done = self._env.step_(action[0].detach().numpy())
+    reward, done = self._env.step_(action.detach())
     state = self._env.get_obs()
-    return torch.tensor(state, dtype=torch.float32).unsqueeze(dim=0), reward, done
+    self.n_step += 1
+    if self.n_step > 60: 
+        self.n_step = 0
+        self.reset()
+    elif done:
+        self.n_step = 0
+    return torch.tensor(state, dtype=torch.float32), reward, done
